733db39bef1ffd57f72d29225e9093f469a231a0
vzvu3k6k <vzvu3k6k@gmail.com>
2015-08-01 11:46:57 +0900

    Adopt JavaScript Standard Style

--- twitter-quote_tweets_with_embedded_tweets.user.js
+++ twitter-quote_tweets_with_embedded_tweets.user.js
@@ -10,45 +10,49 @@
 // @license        CC0
 // ==/UserScript==
 
-var handler = function(event) {
-  var $quoteTweet = event.target, match;
+/* js-standard-style (https://github.com/feross/standard) */
+/* global twttr */
+
+var handler = function (event) {
+  var $quoteTweet = event.target
+  var match
   while ($quoteTweet && $quoteTweet.classList) {
-    if ((match = $quoteTweet.classList.contains('QuoteTweet'))) break;
-    $quoteTweet = $quoteTweet.parentNode;
+    if ((match = $quoteTweet.classList.contains('QuoteTweet'))) break
+    $quoteTweet = $quoteTweet.parentNode
   }
-  if (!match) return;
+  if (!match) return
 
   // An embedded tweet is blanked after expanding a tweet container
   // or pjax page transition.
   // So, we remove blanked embedded tweets here.
   Array.from($quoteTweet.parentNode.querySelectorAll('.twitter-tweet-rendered'))
-    .filter((node) => node.contentDocument.body.childElementCount == 0)
-    .forEach((node) => node.parentNode.remove());
+    .filter((node) => node.contentDocument.body.childElementCount === 0)
+    .forEach((node) => node.parentNode.remove())
 
   // Abort if embedded tweet has been already inserted
-  var $prevSib = $quoteTweet.previousElementSibling;
+  var $prevSib = $quoteTweet.previousElementSibling
   if ($prevSib.dataset.twttrId &&
-      $prevSib.dataset.twttrId.indexOf('twttr-sandbox-') != -1) return;
+      $prevSib.dataset.twttrId.indexOf('twttr-sandbox-') !== -1) return
 
-  var insertTweet = function() {
-    var tweetId = $quoteTweet.querySelector('.js-permalink').dataset.itemId;
-    var $target = document.createElement('div');
-    $quoteTweet.parentNode.insertBefore($target, $quoteTweet);
-    twttr.widgets.createTweet(tweetId, $target);
-  };
+  var insertTweet = function () {
+    var tweetId = $quoteTweet.querySelector('.js-permalink').dataset.itemId
+    var $target = document.createElement('div')
+    $quoteTweet.parentNode.insertBefore($target, $quoteTweet)
+    twttr.widgets.createTweet(tweetId, $target)
+  }
 
   if (!window.twttr) {
     // insertTweet is called when widgets.js is loaded
-    window.twttr = {_e: [insertTweet]};
+    window.twttr = {_e: [insertTweet]}
 
-    var s = document.createElement('script');
-    s.src = '//platform.twitter.com/widgets.js';
-    document.head.appendChild(s);
+    var s = document.createElement('script')
+    s.src = '//platform.twitter.com/widgets.js'
+    document.head.appendChild(s)
   } else {
-    insertTweet();
+    insertTweet()
   }
 
-  event.stopPropagation();
-};
+  event.stopPropagation()
+}
 
-window.addEventListener('click', handler, true);
+window.addEventListener('click', handler, true)
